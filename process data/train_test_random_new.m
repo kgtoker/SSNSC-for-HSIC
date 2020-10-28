3c45f1e249c421f5643970782d77b8c4887ec14f
kgtoker <57569368+kgtoker@users.noreply.github.com>
2020-10-28 12:06:33 +0300

    Update train_test_random_new.m

--- process data/train_test_random_new.m
+++ process data/train_test_random_new.m
@@ -1,19 +1,7 @@
 function [indexes]=train_test_random_new(y,n)
 % function to ramdonly select training samples and testing samples from the
 % whole set of ground truth.
-% alltrain is the ground truth
-% % clc
-% alltrain = alltrain';
-% y = alltrain(2,:); % Indiana
-% y = alltrain(1,:); % Salinas
-% yindex = [];
-% train = [];
-% lys = 0;
-
 K = max(y);
-% pK = 0.0001,
-
-
 % generate the  training set
 indexes = [];
 for i = 1:K
