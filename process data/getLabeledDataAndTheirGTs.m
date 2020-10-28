73617b98288f22d1849dd7bd04f76a577693872f
kgtoker <57569368+kgtoker@users.noreply.github.com>
2020-10-28 12:09:03 +0300

    Update getLabeledDataAndTheirGTs.m

--- process data/getLabeledDataAndTheirGTs.m
+++ process data/getLabeledDataAndTheirGTs.m
@@ -2,14 +2,6 @@ function [allLabeledData, GTVector, dataCascade, no_classes, numberOfInstancesOf
 
 sz = size(img);
 dataCascade = reshape(img, [prod(sz(1:2)) sz(3)])';
-% normalize data
-% dataCascade = dataCascade / max(max(dataCascade));
-%% Do derivative if you want
-% if 1 == DerivativeOn
-%     dataCascade = specDerivative(dataCascade, derivativeValue);
-% end
-
-%     diff_img = reshape(diff_img, [prod(sz(1:2)) sz(3)])';
 GTVector =  [reshape(gt,[sz(1)*sz(2) 1])]';
 
 for i = 1:max(GTVector)
@@ -19,7 +11,6 @@ end
 all = [1:sz(1)*sz(2)];
 allLabeledData = [all;GTVector];
 allLabeledData(:,allLabeledData(2,:)==0) = [];
-%     allLabeledData(:,allLabeledData(2,:)==9) = [];
 no_classes=max(allLabeledData(2,:));
 % imagesc(reshape(gt,[sz(1) sz(2)]));
 % axis image
